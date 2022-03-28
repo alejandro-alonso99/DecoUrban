51718bf31064dcc4c78c8d0124d7f8113156a9c3
Alequita <alejandroalonso0306@gmail.com>
2022-03-28 16:40:45 -0300

    upload file form

--- decourban/base/views.py
+++ decourban/base/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from xml.dom.minidom import Document
+from django.shortcuts import redirect, render
 
 def add_file(request):
+    if request.method == 'POST':
 
+        updated_file = request.FILES['document']
+        print(updated_file.name)
+        print(updated_file.size)
+    
     return render(request, 'base/add_file.html',{})
